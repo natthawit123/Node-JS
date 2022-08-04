2b027a2495934f490e1224bfc0afdc4e3b78ff47
Natthawit <110612584+natthawit123@users.noreply.github.com>
2022-08-04 21:31:44 +0700

    Update app.js 1

--- app.js
+++ app.js
@@ -2,22 +2,16 @@ const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const path = require('path');
-
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
-
-app.set("visws","./src/views");
-app.set("visw engine","ejs")
 
 app.get("/", (req,res) =>{
 
-    res.send('index',{username: 'Natthawt+', customers: ["tag11","tag12","tag13"]});
+    res.send('Hello Word 12');
 })
 
 app.listen(port, ()=>{
-    debug("Listening on port " + chalk.blue(" : "+port));
+    debug("Listening on port " + chalk.blue(port));
 })

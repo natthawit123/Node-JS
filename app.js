6c43e48216ec164eb819ff469429a1a37c08f09e
Natthawit <110612584+natthawit123@users.noreply.github.com>
2022-08-04 22:05:01 +0700

    Add all files
    Hello World :-1:

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req,res) =>{
 
-    res.send('Hello Word 12');
+    res.send('Hello Word 1');
 })
 
 app.listen(port, ()=>{

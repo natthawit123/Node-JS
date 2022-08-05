69863f1ec6c3721e22477ab2ca6faadc37c3ec6a
Natthawit <110612584+natthawit123@users.noreply.github.com>
2022-08-05 17:10:20 +0700

    Update new1

--- public/inc/woocommerce.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Add WooCommerce support
- */
-
-add_action( 'after_setup_theme', 'serenity_lite_woocommerce_support' );
-function serenity_lite_woocommerce_support() {
-    add_theme_support( 'woocommerce' );
-}
